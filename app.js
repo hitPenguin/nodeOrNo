a4d297e30c10f7e1d817bc422f36253dfbb3199f
chen qiang <chenqiang@unitedstack.com>
2018-11-05 14:55:06 +0800

    delete more

--- app.js
+++
@@ -1,7 +0,0 @@
-console.log(process.env.NODE_CHANNEL_FD);
-
-console.log(process.NODE_CHANNEL_FD);
-
-console.log(process.env.NODE_UNIQUE_ID);
-
-console.log(process.NODE_UNIQUE_ID);
--- index.js
+++
@@ -1,8 +0,0 @@
-const cp = require('child_process');
-
-const worker = cp.fork('./app.js', [], { stdio: 'inherit' });
-
-// console.log(worker.env.NODE_CHANNEL_FD);
-
-console.log(worker.NODE_CHANNEL_FD);
-
